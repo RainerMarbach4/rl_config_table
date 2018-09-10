a66b2b3f193a25e5ea212ddaa9eeb94df9ca625c
Rainer Langheiter <rainer@langheiter.at>
2018-09-10 22:32:47 +0200

    copyright

--- NEUE DATEIEN/DEINADMIN/rl_config_table.php
+++ NEUE DATEIEN/DEINADMIN/rl_config_table.php
@@ -1,7 +1,8 @@
 <?php
 /**
 * @package rl_config_table
-* @copyright Copyright 2016-2018 rainer@langheiter.com
+* Author:  rainer@langheiter.com  https://langheiter.com
+*          david@langheiter.com
 * @copyright Copyright 2003-2018 Zen Cart Development Team
 * @copyright Portions Copyright 2003 osCommerce
 * @license http://www.zen-cart-pro.at/license/2_0.txt GNU Public License V2.0
